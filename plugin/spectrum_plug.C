2766187a852f56af0f1c578fd62c9b6c37b68d98
ghobbs <>
2009-01-12 04:01:19 +0000

    Added ability to plot spectra of multiple pulsars

--- plugin/spectrum_plug.C
+++ plugin/spectrum_plug.C
@@ -14,10 +14,11 @@ using namespace std;
 #define MAX_ID 50
 
 void drawOption(int on,float x,float y,char *str);
-void doPlugin(pulsar *psr,double *x,double *y,int n);
+void doPlugin(pulsar *psr,int npsr);
 void drawMenu(int specType,int xaxis,int logv,int specOut);
 void checkMenu(float mx,float my,int *change,int *xaxis,int *logv,int *specType,int *specOut);
-void identify(float mx,float my,float *px,float *py,int sn, int idV[MAX_ID],int *iN);
+void identify(float mx,float my,float px[MAX_PSR_VAL][MAX_OBSN_VAL],float py[MAX_PSR_VAL][MAX_OBSN_VAL],
+	      int *sn, int idV[MAX_ID],int idP[MAX_ID], int *iN,int npsr);
 
 void help() /* Display help */
 {
@@ -43,9 +44,9 @@ extern "C" int graphicalInterface(int argc,char *argv[],pulsar *psr,int *npsr)
   char parFile[MAX_PSR][MAX_FILELEN];
   char timFile[MAX_PSR][MAX_FILELEN];
   int i,n=0;
-  double px[MAX_OBSN],py[MAX_OBSN];
+  double px[MAX_OBSN],py[MAX_OBSN],pe[MAX_OBSN];
 
-  *npsr = 1;  /* For a graphical interface that only shows results for one pulsar */
+  *npsr = 0;  /* For a graphical interface that only shows results for one pulsar */
 
   printf("Graphical Interface: spectrum\n");
   printf("Author:              G. Hobbs\n");
@@ -53,22 +54,15 @@ extern "C" int graphicalInterface(int argc,char *argv[],pulsar *psr,int *npsr)
   printf(" --- type 'h' for help information\n");
 
 
-  /* Obtain the .par and the .tim file from the command line */
-  if (argc==4) /* Only provided .tim name */
-    {
-      strcpy(timFile[0],argv[3]);
-      strcpy(parFile[0],argv[3]);
-      parFile[0][strlen(parFile[0])-3] = '\0';
-      strcat(parFile[0],"par");
-    }
 
   /* Obtain all parameters from the command line */
   for (i=2;i<argc;i++)
     {
       if (strcmp(argv[i],"-f")==0)
 	{
-	  strcpy(parFile[0],argv[i+1]); 
-	  strcpy(timFile[0],argv[i+2]);
+	  strcpy(parFile[*npsr],argv[i+1]); 
+	  strcpy(timFile[*npsr],argv[i+2]);
+	  (*npsr)++;
 	}
     }
 
@@ -83,32 +77,24 @@ extern "C" int graphicalInterface(int argc,char *argv[],pulsar *psr,int *npsr)
       if (i==0) doFit(psr,*npsr,0);   /* Do the fitting     */
       else textOutput(psr,*npsr,0,0,0,0,"");  /* Display the output */
     }
-  n=0;
-  for (i=0;i<psr[0].nobs;i++)
-    {
-      if (psr[0].obsn[i].deleted==0)
-	{
-	  px[n] = (double)(psr[0].obsn[i].sat-psr[0].param[param_pepoch].val[0]);
-	  py[n] = (double)psr[0].obsn[i].residual;
-	  //	  py[n] = 3*sin(2.0*M_PI/2041.34587909003*10*px[n]);
-	  n++;
-	}
-    }
 
-  doPlugin(psr,px,py,n);
+  doPlugin(psr,*npsr);
 
   return 0;
 }
 
-void doPlugin(pulsar *psr,double *resx,double *resy,int n)
+void doPlugin(pulsar *psr,int npsr)
 {
-  float px[MAX_OBSN],py[MAX_OBSN];
+  float px[MAX_PSR_VAL][MAX_OBSN_VAL],py[MAX_PSR_VAL][MAX_OBSN_VAL];
+  double resx[MAX_OBSN],resy[MAX_OBSN],rese[MAX_OBSN];
+  int n;
   float minx,maxx,miny,maxy;
   double specX[MAX_OBSN],specY[MAX_OBSN];
   float mouseX,mouseY;
+  int p;
   char key;
   int specType=2;
-  int specN;
+  int specN[MAX_PSR];
   int xaxis=3;
   int endit=0;
   int change=1;
@@ -116,7 +102,7 @@ void doPlugin(pulsar *psr,double *resx,double *resy,int n)
   int plotPoints=-1;
   int plotLine=1;
   int i;
-  int idV[MAX_ID];
+  int idV[MAX_ID],idP[MAX_ID];
   int iN=0;
   int fontSize=1;
   char str[100];
@@ -139,60 +125,90 @@ void doPlugin(pulsar *psr,double *resx,double *resy,int n)
     
     if (change==1)
       {
-	TKspectrum(resx,resy,n,0,0,0,0,0,specType,ofac,specOut,specX,specY,&specN,0,0);
-	TKconvertFloat2(specX,specY,px,py,specN);
-	if (xaxis==1) // Convert x-axis to s^-1
+	for (p=0;p<npsr;p++)
 	  {
-	    for (i=0;i<specN;i++)
-	      px[i]/=SECDAY;
+	    n=0;
+	    for (i=0;i<psr[p].nobs;i++)
+	      {
+		if (psr[p].obsn[i].deleted==0)
+		  {
+		    resx[n] = (double)(psr[p].obsn[i].sat-psr[p].param[param_pepoch].val[0]);
+		    resy[n] = (double)psr[p].obsn[i].residual;
+		    rese[n] = (double)psr[p].obsn[i].toaErr*1.0e-6;
+		    //	  py[n] = 3*sin(2.0*M_PI/2041.34587909003*10*px[n]);
+		    n++;
+		  }
+	      }
+
+	    TKspectrum(resx,resy,rese,n,0,0,0,0,0,specType,ofac,specOut,specX,specY,&specN[p],0,0);
+	    TKconvertFloat2(specX,specY,px[p],py[p],specN[p]);
+	    if (xaxis==1) // Convert x-axis to s^-1
+	      {
+		for (i=0;i<specN[p];i++)
+		  px[p][i]/=SECDAY;
+	      }
+	    else if (xaxis==3) // Convert x-axis to yr^-1
+	      {
+		for (i=0;i<specN[p];i++)
+		  px[p][i]*=365.25;
+	      }
+	    else if (xaxis==4) // Convert x-axis to s
+	      {
+		for (i=0;i<specN[p];i++)
+		  px[p][i]=SECDAY/px[p][i];
+	      }
+	    else if (xaxis==5) // Convert x-axis to d
+	      {
+		for (i=0;i<specN[p];i++)
+		  px[p][i]=1.0/px[p][i];
+	      }
+	    else if (xaxis==6) // Convert x-axis to d
+	      {
+		for (i=0;i<specN[p];i++)
+		  px[p][i]=1.0/px[p][i]/365.25;
+	      }
+	  
+	    if (logv > 0 && change==1)
+	      {
+		for (i=0;i<specN[p];i++)
+		  {
+		    if (logv==1 || logv==3)
+		      px[p][i] = log10(px[p][i]);
+		    if (logv==2 || logv==3)
+		      py[p][i] = log10(py[p][i]);
+		  }
+	      }
 	  }
-	else if (xaxis==3) // Convert x-axis to yr^-1
-	  {
-	    for (i=0;i<specN;i++)
-	      px[i]*=365.25;
-	  }
-	else if (xaxis==4) // Convert x-axis to s
-	  {
-	    for (i=0;i<specN;i++)
-	      px[i]=SECDAY/px[i];
-	  }
-	else if (xaxis==5) // Convert x-axis to d
-	  {
-	    for (i=0;i<specN;i++)
-	      px[i]=1.0/px[i];
-	  }
-	else if (xaxis==6) // Convert x-axis to d
-	  {
-	    for (i=0;i<specN;i++)
-	      px[i]=1.0/px[i]/365.25;
-	  }
-      }
-    if (logv > 0 && change==1)
-      {
-	for (i=0;i<n;i++)
-	  {
-	    if (logv==1 || logv==3)
-	      px[i] = log10(px[i]);
-	    if (logv==2 || logv==3)
-	      py[i] = log10(py[i]);
-	  }
-      }
-    if (zoom==0 || change==1)
-      {
-	if (logv==0)
-	  {
-	    minx = 0.0;
-	    miny = 0.0;
-	  }
-	else
-	  {
-	    minx = TKfindMin_f(px,specN);
-	    miny = TKfindMin_f(py,specN);
-	  }
-	maxx = TKfindMax_f(px,specN);
-	maxy = TKfindMax_f(py,specN);
       }
 
+    for (p=0;p<npsr;p++)
+      {
+	if (p==0)
+	  {
+	    minx = maxx = px[0][0];
+	    miny = maxy = py[0][0];
+	  }
+	if (zoom==0 || change==1)
+	  {
+	    for (i=0;i<specN[p];i++)
+	      {
+		if (logv==0)
+		  {
+		    minx = 0.0;
+		    miny = 0.0;
+		  }
+		else
+		  {
+		    if (minx > px[p][i]) minx = px[p][i];
+		    if (miny > py[p][i]) miny = py[p][i];
+		  }
+		if (maxx < px[p][i]) maxx = px[p][i];
+		if (maxy < py[p][i]) maxy = py[p][i];
+	      }
+	  }
+      }
+  
+  
     cpgswin(minx,maxx,miny,maxy+0.1*(maxy-miny));
     if (logv==0)
       cpgbox("BCNST1",0.0,0,"BCNST1",0.0,0);
@@ -202,32 +218,35 @@ void doPlugin(pulsar *psr,double *resx,double *resy,int n)
       cpgbox("BCNST1",0.0,0,"BCNST1L",0.0,0);
     else if (logv==3)
       cpgbox("BCNST1L",0.0,0,"BCNST1L",0.0,0);
-
+    
     if (specOut==1) strcpy(ylabel,"PSD");
     else if (specOut==2) strcpy(ylabel,"Amplitude");
     else if (specOut==3) strcpy(ylabel,"Power");
-
+    
     if (xaxis==1) cpglab("Frequency (s\\u-1\\d)",ylabel,"");
     else if (xaxis==2) cpglab("Frequency (d\\u-1\\d)",ylabel,"");
     else if (xaxis==3) cpglab("Frequency (yr\\u-1\\d)",ylabel,"");
     else if (xaxis==4) cpglab("Sec",ylabel,"");
     else if (xaxis==5) cpglab("Day",ylabel,"");
     else if (xaxis==6) cpglab("Yr",ylabel,"");
-    if (plotLine==1)
-      cpgline(specN,px,py);
-    if (plotPoints==1)
-      cpgpt(specN,px,py,9);
-
+    for (p=0;p<npsr;p++)
+      {
+	cpgsci(p+1);
+	if (plotLine==1)
+	  cpgline(specN[p],px[p],py[p]);
+	if (plotPoints==1)
+	  cpgpt(specN[p],px[p],py[p],9);
+      }
     // Label identified points
     cpgsch(fontSize/1.8);
     cpgqcs(4,&chx,&chy);
     cpgsci(3);
     for (i=0;i<iN;i++)
       {
-	sprintf(str,"id = %d",i+1); cpgtext(px[idV[i]]+chx,py[idV[i]]+chy,str);	
-	sprintf(str,"index = %d",idV[i]+1); cpgtext(px[idV[i]]+chx,py[idV[i]],str);	
-	sprintf(str,"(%.2g,%-.2g)",px[idV[i]],py[idV[i]]); cpgtext(px[idV[i]]+chx,py[idV[i]]-chy,str);	
-	printf("plotting at %g %d\n",px[idV[i]],idV[i]);
+	sprintf(str,"id = %d",i+1); cpgtext(px[idP[i]][idV[i]]+chx,py[idP[i]][idV[i]]+chy,str);	
+	sprintf(str,"index = %d",idV[i]+1); cpgtext(px[idP[i]][idV[i]]+chx,py[idP[i]][idV[i]],str);	
+	sprintf(str,"(%.2g,%-.2g)",px[idP[i]][idV[i]],py[idP[i]][idV[i]]); 
+	cpgtext(px[idP[i]][idV[i]]+chx,py[idP[i]][idV[i]]-chy,str);	
       }
     cpgsci(1);
     cpgsch(fontSize);
@@ -238,7 +257,7 @@ void doPlugin(pulsar *psr,double *resx,double *resy,int n)
     checkMenu(mouseX,mouseY,&change,&xaxis,&logv,&specType,&specOut);
     if (key=='q') endit=1;
     else if (key=='A' && change==0) // Identify point 
-      identify(mouseX,mouseY,px,py,specN,idV,&iN);
+      identify(mouseX,mouseY,px,py,specN,idV,idP,&iN,npsr);
     else if (key=='u') // Unzoom
       zoom=0;
     else if (key=='h') // Help
@@ -255,8 +274,11 @@ void doPlugin(pulsar *psr,double *resx,double *resy,int n)
       plotLine*=-1;
     else if (key=='l') // List points
       {
-	for (i=0;i<specN;i++)
-	  printf("%d %g %g\n",i+1,px[i],py[i]);
+	for (p=0;p<npsr;p++)
+	  {
+	    for (i=0;i<specN[p];i++)
+	      printf("%d %d %g %g\n",p+1,i+1,px[p][i],py[p][i]);
+	  }
       }
     else if (key=='z') // Zoom
       {
@@ -284,10 +306,10 @@ void doPlugin(pulsar *psr,double *resx,double *resy,int n)
   cpgend();
 }
 
-void identify(float mx,float my,float *px,float *py,int sn,
-	      int idV[MAX_ID],int *iN)
+void identify(float mx,float my,float px[MAX_PSR_VAL][MAX_OBSN_VAL],float py[MAX_PSR_VAL][MAX_OBSN_VAL],
+	      int *sn, int idV[MAX_ID],int idP[MAX_ID], int *iN,int npsr)
 {
-  int i,iclosest;
+  int i,iclosest,p,pclosest;
   float closest,x1,x2,x3,x4,y1,y2,y3,y4,xscale,yscale,xpos,ypos;
 
   cpgqvp(3,&x1,&x2,&y1,&y2);
@@ -297,24 +319,31 @@ void identify(float mx,float my,float *px,float *py,int sn,
   mx = (mx-x3)*xscale;
   my = (my-y3)*yscale;
   iclosest=-1;
-  for (i=0;i<sn;i++)
+  pclosest=-1;
+  for (p=0;p<npsr;p++)
     {
-      xpos = (px[i]-x3)*xscale;
-      ypos = (py[i]-y3)*yscale;
-      if (iclosest==-1)
+      for (i=0;i<sn[p];i++)
 	{
-	  iclosest=i;
-	  closest = pow(xpos-mx,2)+pow(ypos-my,2);
-	}
-      else if (pow(xpos-mx,2)+pow(ypos-my,2)<closest)
-	{
-	  iclosest=i;
-	  closest = pow(xpos-mx,2)+pow(ypos-my,2);
+	  xpos = (px[p][i]-x3)*xscale;
+	  ypos = (py[p][i]-y3)*yscale;
+	  if (iclosest==-1)
+	    {
+	      iclosest=i;
+	      pclosest=p;
+	      closest = pow(xpos-mx,2)+pow(ypos-my,2);
+	    }
+	  else if (pow(xpos-mx,2)+pow(ypos-my,2)<closest)
+	    {
+	      iclosest=i;
+	      pclosest=p;
+	      closest = pow(xpos-mx,2)+pow(ypos-my,2);
+	    }
 	}
     }
   printf("Closest point has index %d (starting from 1)\n",iclosest+1);
-  printf("This has coordinates (%g,%g)\n",px[iclosest],py[iclosest]);
+  printf("This has coordinates (%g,%g)\n",px[p][iclosest],py[p][iclosest]);
   idV[*iN] = iclosest;
+  idP[*iN] = pclosest;
   (*iN)++;
 }
 
@@ -323,6 +352,8 @@ void drawMenu(int specType,int xaxis,int logv,int specOut)
   drawOption(specType,0.0,0.8,"DFT");
   drawOption(specType-1,0.1,0.8,"Lomb");
   drawOption(specType-2,0.2,0.8,"FFT");
+  drawOption(specType-3,0.3,0.8,"WLS");
+  drawOption(specType-4,0.4,0.8,"Sine");
   drawOption(xaxis,0.0,0.6,"s\\u-1\\d");
   drawOption(xaxis-1,0.1,0.6,"d\\u-1\\d");
   drawOption(xaxis-2,0.2,0.6,"yr\\u-1\\d");  
@@ -373,6 +404,8 @@ void checkMenu(float mx,float my,int *change,int *xaxis,int *logv,int *specType,
 	  if (mouseX==0) *specType=1;
 	  else if (mouseX==1) *specType=2;
 	  else if (mouseX==2) *specType=3;
+	  else if (mouseX==3) *specType=4;
+	  else if (mouseX==4) *specType=5;
 	}
       if (mouseY==1)
 	{
