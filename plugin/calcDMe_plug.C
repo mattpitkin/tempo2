a95ea3b32be5aa9ddcae9fc8b52aea97b2432926
sosl <>
2009-04-09 17:06:53 +0000

    Added GPL license and copyrighted all files - most of them now are copyrighted by Stefan Oslowski with rights belonging to  George Hobbs and Russel Edwards. If you think some of the files should belong to you, please notify Stefan ( soslowski@astro.swin.edu.au ) or George.

--- plugin/calcDMe_plug.C
+++ plugin/calcDMe_plug.C
@@ -1,19 +1,28 @@
-// Copyright (C) 2009 Stefan Oslowski, George Hobbs
+//  Copyright (C) 2006,2007,2008,2009, Stefan Oslowski, George Hobbs, Russel Edwards
 
-/* This file is part of Tempo2
- *
- *  Tempo2 is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/*
+*    This file is part of TEMPO2. 
+* 
+*    TEMPO2 is free software: you can redistribute it and/or modify 
+*    it under the terms of the GNU General Public License as published by 
+*    the Free Software Foundation, either version 3 of the License, or 
+*    (at your option) any later version. 
+*    TEMPO2 is distributed in the hope that it will be useful, 
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+*    GNU General Public License for more details. 
+*    You should have received a copy of the GNU General Public License 
+*    along with TEMPO2.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+/*
+*    If you use TEMPO2 then please acknowledge it by citing 
+*    Hobbs, Edwards & Manchester (2006) MNRAS, Vol 369, Issue 2, 
+*    pp. 655-672 (bibtex: 2006MNRAS.369..655H)
+*    or Edwards, Hobbs & Manchester (2006) MNRAS, VOl 372, Issue 4,
+*    pp. 1549-1574 (bibtex: 2006MNRAS.372.1549E) when discussing the
+*    timing model.
+*/
 
 /* This plugin was written by Stefan Oslowski (CAS, Swinburne) 
  * and George Hobbs (ATNF, CSIRO)
--- plugin/delays_plug.C
+++ plugin/delays_plug.C
@@ -1,3 +1,29 @@
+//  Copyright (C) 2004,2006,2007,2008,2009, George Hobbs, Russel Edwards
+
+/*
+*    This file is part of TEMPO2. 
+* 
+*    TEMPO2 is free software: you can redistribute it and/or modify 
+*    it under the terms of the GNU General Public License as published by 
+*    the Free Software Foundation, either version 3 of the License, or 
+*    (at your option) any later version. 
+*    TEMPO2 is distributed in the hope that it will be useful, 
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+*    GNU General Public License for more details. 
+*    You should have received a copy of the GNU General Public License 
+*    along with TEMPO2.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+/*
+*    If you use TEMPO2 then please acknowledge it by citing 
+*    Hobbs, Edwards & Manchester (2006) MNRAS, Vol 369, Issue 2, 
+*    pp. 655-672 (bibtex: 2006MNRAS.369..655H)
+*    or Edwards, Hobbs & Manchester (2006) MNRAS, VOl 372, Issue 4,
+*    pp. 1549-1574 (bibtex: 2006MNRAS.372.1549E) when discussing the
+*    timing model.
+*/
+
 /* On linux: g++-3.2.3 -fPIC -c delays_plug.C -I/usr/local/pgplot -I/usr/local/include ; g77 -shared -o $TEMPO2PLUG/delays_linux_plug.so delays_plug.o -L/packages/linux/pgplot/ -lcpgplot -lpgplot -ltk8.0 -ltcl8.0 -L/usr/X11R6/lib -lX11 -lpng -lz -lg2c
 
 /* On sungcc: g++-3.1.1 -fPIC -c delays_plug.C ; g++-3.1.1 -shared -o ../delays_sungcc_plug.so delays_plug.o -lcpgplot -lpgplot -lM77 -lF77 -lsunmath */
