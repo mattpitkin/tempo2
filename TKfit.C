8c9dd74f6cdee6d10b54bd2a35f5d7e2c2649be7
ghobbs <>
2009-06-16 02:06:42 +0000

    Fixed bug in calculating chisq

--- TKfit.C
+++ TKfit.C
@@ -1,23 +1,23 @@
-//  Copyright (C) 2006,2007,2008,2009, George Hobbs, Russel Edwards
+//  Copyright (C) 2006,2007,2008,2009, George Hobbs, Russell Edwards
 
 /*
-*    This file is part of TEMPO2. 
-* 
-*    TEMPO2 is free software: you can redistribute it and/or modify 
-*    it under the terms of the GNU General Public License as published by 
-*    the Free Software Foundation, either version 3 of the License, or 
-*    (at your option) any later version. 
-*    TEMPO2 is distributed in the hope that it will be useful, 
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of 
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-*    GNU General Public License for more details. 
-*    You should have received a copy of the GNU General Public License 
-*    along with TEMPO2.  If not, see <http://www.gnu.org/licenses/>. 
+*    This file is part of TEMPO2.
+*
+*    TEMPO2 is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*    TEMPO2 is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*    You should have received a copy of the GNU General Public License
+*    along with TEMPO2.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
-*    If you use TEMPO2 then please acknowledge it by citing 
-*    Hobbs, Edwards & Manchester (2006) MNRAS, Vol 369, Issue 2, 
+*    If you use TEMPO2 then please acknowledge it by citing
+*    Hobbs, Edwards & Manchester (2006) MNRAS, Vol 369, Issue 2,
 *    pp. 655-672 (bibtex: 2006MNRAS.369..655H)
 *    or Edwards, Hobbs & Manchester (2006) MNRAS, VOl 372, Issue 4,
 *    pp. 1549-1574 (bibtex: 2006MNRAS.372.1549E) when discussing the
@@ -151,7 +151,7 @@ void TKleastSquares_svd_psr(double *x,double *y,double *sig,int n,double *p,doub
   *chisq = 0.0;
   for (j=0;j<n;j++)
     {
-      fitFuncs(x[i],basisFunc,nf,psr,ip[i]);
+      fitFuncs(x[j],basisFunc,nf,psr,ip[j]);
       sum=0.0;
       for (k=0;k<nf;k++)
 	{
@@ -159,7 +159,7 @@ void TKleastSquares_svd_psr(double *x,double *y,double *sig,int n,double *p,doub
 	  if (j==0)free(v[k]);
 	}
       (*chisq) += pow((y[j]-sum)/sig[j],2);
-
+  
       free(designMatrix[j]); // Remove memory allocation
       free(u[j]);
       
@@ -169,7 +169,6 @@ void TKleastSquares_svd_psr(double *x,double *y,double *sig,int n,double *p,doub
       for (j=0;j<nf;j++)
 	e[j] *= sqrt(*chisq/(n-nf));
     }
-
   free(v); 
   free(u); free(designMatrix);
 }
