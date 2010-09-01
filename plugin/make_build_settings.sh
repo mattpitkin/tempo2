7b75a0a33b8c3c57f081dbb1a7688f8f17e30c84
sixbynine <>
2010-09-01 03:11:30 +0000

    Better building of plugins

--- plugin/make_build_settings.sh
+++ plugin/make_build_settings.sh
@@ -9,7 +9,7 @@ for lib in `grep -v -e '^#' $list` ; do
 	if [ -e $lib ] ; then
 		/bin/echo -n " "`/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2.la:'`
 	else
-		echo "WARNING: File plugin/$lib does not exist... cannot build plugin" >&2
+		echo "WARNING: File plugin/$lib does not exist... this plugin will not be built" >&2
 	fi
 done
 
@@ -17,12 +17,11 @@ done
 
 for lib in `grep -v -e '^#' $list` ; do
 	if [ -e $lib ] ; then
-		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_SOURCES=../tempo2.h '$lib':'
-		if [ -n "$libextra" ] ; then
-			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_LIBADD='$libextra':'
-		fi
+		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_SOURCES=../tempo2.h '"$lib"':'
+		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_LIBADD=../libtempo2.la '"$libextra"':'
 		if [ -n "$incextra" ] ; then
-			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_INCLUDES='$incextra':'
+			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_INCLUDES='"$incextra"':'
 		fi
 	fi
 done
+
