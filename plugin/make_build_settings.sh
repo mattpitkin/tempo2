d713e493a4e2d8b44cba99cceedc2dbf4ed84a48
sixbynine <>
2010-09-01 03:06:16 +0000

    Fixing some missing updates. Allowing build of "splug" plugins.

--- plugin/make_build_settings.sh
+++ plugin/make_build_settings.sh
@@ -1,21 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 list=$1
 libextra=$2
 incextra=$3
 
+
 /bin/echo -n "plugin_LTLIBRARIES+="
 for lib in `grep -v -e '^#' $list` ; do
-	/bin/echo -n " "`/bin/echo $lib | sed -e 's:\(.*\)_plug\..*:\1_@T2ARCH@_plug.la:'`
+	if [ -e $lib ] ; then
+		/bin/echo -n " "`/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2.la:'`
+	else
+		echo "WARNING: File plugin/$lib does not exist... cannot build plugin" >&2
+	fi
 done
 
 /bin/echo ""
 
 for lib in `grep -v -e '^#' $list` ; do
-	/bin/echo $lib | sed -e 's:\(.*\)_plug\..*:\1_@T2ARCH@_plug_la_SOURCES=../tempo2.h '$lib':'
-	if [ -n "$libextra" ] ; then
-		/bin/echo $lib | sed -e 's:\(.*\)_plug\..*:\1_@T2ARCH@_plug_la_LIBADD='$libextra':'
-	fi
-	if [ -n "$incextra" ] ; then
-		/bin/echo $lib | sed -e 's:\(.*\)_plug\..*:\1_@T2ARCH@_plug_la_INCLUDES='$incextra':'
+	if [ -e $lib ] ; then
+		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_SOURCES=../tempo2.h '$lib':'
+		if [ -n "$libextra" ] ; then
+			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_LIBADD='$libextra':'
+		fi
+		if [ -n "$incextra" ] ; then
+			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_INCLUDES='$incextra':'
+		fi
 	fi
 done
