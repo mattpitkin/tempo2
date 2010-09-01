7434771ee7b1fa3606b21b13b0f8ccb99a4f699a
sixbynine <>
2010-09-01 03:24:54 +0000

    Fixing sed command to run on MacOSX

--- plugin/make_build_settings.sh
+++ plugin/make_build_settings.sh
@@ -7,7 +7,7 @@ incextra=$3
 /bin/echo -n "plugin_LTLIBRARIES+="
 for lib in `grep -v -e '^#' $list` ; do
 	if [ -e $lib ] ; then
-		/bin/echo -n " "`/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2.la:'`
+		/bin/echo -n " "`/bin/echo $lib | sed -e 's:\(.*\)_\(s\{0,1\}plug\)\..*:\1_@T2ARCH@_\2.la:'`
 	else
 		echo "WARNING: File plugin/$lib does not exist... this plugin will not be built" >&2
 	fi
@@ -17,10 +17,10 @@ done
 
 for lib in `grep -v -e '^#' $list` ; do
 	if [ -e $lib ] ; then
-		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_SOURCES=../tempo2.h '"$lib"':'
-		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_LIBADD=../libtempo2.la '"$libextra"':'
+		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{0,1\}plug\)\..*:\1_@T2ARCH@_\2_la_SOURCES=../tempo2.h '"$lib"':'
+		/bin/echo $lib | sed -e 's:\(.*\)_\(s\{0,1\}plug\)\..*:\1_@T2ARCH@_\2_la_LIBADD=../libtempo2.la '"$libextra"':'
 		if [ -n "$incextra" ] ; then
-			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{,1\}plug\)\..*:\1_@T2ARCH@_\2_la_INCLUDES='"$incextra"':'
+			/bin/echo $lib | sed -e 's:\(.*\)_\(s\{0,1\}plug\)\..*:\1_@T2ARCH@_\2_la_INCLUDES='"$incextra"':'
 		fi
 	fi
 done
