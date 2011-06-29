e23214a344c3d5305f80efc16b00f37f76cbeadc
sixbynine <>
2011-06-29 06:08:46 +0000

    Fixed bug where it was using an unasigned variable when changing position epoch.
    Sometimes it worked, since the value should be 0 and so often it defaults to 0

--- preProcess.C
+++ preProcess.C
@@ -322,7 +322,7 @@ void preProcess(pulsar *psr,int npsr,int argc,char *argv[])
 	      char retstr[1000];
 	      psr[p].param[param_decj].val[0] = psr[p].param[param_decj].val[0]+psr[p].param[param_pmdec].val[0]/1000.0*(M_PI/180.0)/60.0/60.0*dt; 
 	      psr[p].param[param_decj].prefit[0] = psr[p].param[param_decj].val[0];
-	      turn_dms(psr[p].param[param_decj].val[k]/(2.0*M_PI), retstr);
+	      turn_dms(psr[p].param[param_decj].val[0]/(2.0*M_PI), retstr);
 	      strcpy(psr[p].decjStrPost,retstr);
 	      strcpy(psr[p].decjStrPre,retstr);
 	    }
