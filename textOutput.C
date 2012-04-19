950769c5aa5f204435b29faa04727f8780468507
ghobbs <>
2012-04-19 22:27:11 +0000

    Fixed bug identified by S. Buchner where the jump flags were not automatically set in the .par file when using JUMP MJD or JUMP FREQ

--- textOutput.C
+++ textOutput.C
@@ -938,7 +938,7 @@ void textOutput(pulsar *psr,int npsr,double globalParameter,int nGlobal,int outR
 	    {
 	      nread = sscanf(psr[p].jumpStr[i],"%s %s %s %s %s",str1,str2,str3,str4,str5);
 	      if (strcasecmp(str1,"FREQ")==0 || strcasecmp(str1,"MJD")==0)
-		fprintf(fout2,"JUMP %s %s %s %.14g 1\n",str1,str2,str3,psr[p].jumpVal[i]);
+		fprintf(fout2,"JUMP %s %s %s %.14g %d\n",str1,str2,str3,psr[p].jumpVal[i],psr[p].fitJump[i]);
 	      else if (strcasecmp(str1,"NAME")==0 || strcasecmp(str1,"TEL")==0 || str1[0]=='-')
 		fprintf(fout2,"JUMP %s %s %.14g %d\n",str1,str2,psr[p].jumpVal[i],psr[p].fitJump[i]);
 	    }	  
