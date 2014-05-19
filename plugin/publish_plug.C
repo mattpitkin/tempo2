092fc51159cd0891d3cefcae3af6809c3c432c5d
ghobbs <>
2014-05-19 04:31:49 +0000

    Various updates

--- plugin/publish_plug.C
+++ plugin/publish_plug.C
@@ -6,12 +6,14 @@
 #include <math.h>
 #include "tempo2.h"
 
-void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac);
+void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac,int useCompare,char *compareFile,int *useDagger);
 int nint_derived(double x);
 int rnd8(double rval,double rerr,int ifac,char *cval,int *lv,char *cerr,
          int *le,char *msg);
 void parseMinus(char *str);
 void parseExp(char *str);
+double fixRA(char *tstr,double err,char *valStr);
+double fixDec(char *tstr,double err,char *valStr);
 
 extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr) 
 {  
@@ -22,6 +24,12 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
   double efac=1.0;
   char efacStr[500];
   int nohead=0;
+  char compareFile[1024];
+  int useCompare=0;
+  int useDagger=0;
+  char dist1[1024]="NULL";
+  char dist2[1024]="NULL";
+  double pdot;
 
   for (i=0;i<argc;i++)
     {
@@ -29,6 +37,15 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
         sscanf(argv[i+1],"%lf",&efac);
       else if (strcmp(argv[i],"-nohead")==0)
 	nohead=1;
+      else if (strcmp(argv[i],"-dist_tc")==0)
+	strcpy(dist1,argv[++i]);
+      else if (strcmp(argv[i],"-dist_cl")==0)
+	strcpy(dist2,argv[++i]);
+      else if (strcmp(argv[i],"-compare")==0)
+	{
+	  useCompare=1;
+	  strcpy(compareFile,argv[++i]);
+	}
     }
   char format[20]="";
   textOutput(psr,npsr,0,0,0,0,format);
@@ -38,14 +55,30 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
     fprintf(fout,"\\documentclass{article}\n");
     fprintf(fout,"\\begin{document}\n");
   }
-  fprintf(fout,"\\begin{table}\n");
-  fprintf(fout,"\\caption{Parameters for PSR %s}\n",psr[0].name);
+  if (useCompare==1)
+    fprintf(fout,"\\begin{table*}\n");
+  else
+    fprintf(fout,"\\begin{table}\n");
+
+  strcpy(name,psr[0].name); parseMinus(name);
+  fprintf(fout,"\\caption{Parameters for PSR %s}\n",name);
   if (nohead==1){
     fprintf(fout,"\\resizebox{35pc}{!}{\\begin{minipage}{\\textwidth}\n");
   }
-  fprintf(fout,"\\begin{tabular}{ll}\n");
+  if (useCompare==1)
+    fprintf(fout,"\\begin{tabular}{llll}\n");
+  else
+    fprintf(fout,"\\begin{tabular}{ll}\n");
   fprintf(fout,"\\hline\\hline\n");
-  fprintf(fout,"\\multicolumn{2}{c}{Fit and data-set} \\\\\n");
+  if (useCompare==1)
+    {
+      fprintf(fout,"\\multicolumn{2}{c}{Fit and data-set} \\\\\n");
+      fprintf(fout,"& This work & Previous & Change \\\\\n");
+      fprintf(fout,"&           &          & ($\\sigma$) \\\\\n");
+    }
+  else
+    fprintf(fout,"\\multicolumn{2}{c}{Fit and data-set} \\\\\n");
+  
   fprintf(fout,"\\hline\n");
   strcpy(name,psr[0].name); parseMinus(name);
   // Check if a J already exists in the name
@@ -92,7 +125,7 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
           if (psr[0].param[i].paramSet[k]==1 && 
               psr[0].param[i].fitFlag[k]==1 && 
               i!=param_start && i!=param_finish){
-            dispParameter(i,k,psr,fout,1,efac);
+            dispParameter(i,k,psr,fout,1,efac,useCompare,compareFile,&useDagger);
           }
         }
     }
@@ -117,7 +150,7 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
             {
               if (psr[0].param[i].paramSet[k]==1 && 
                   psr[0].param[i].fitFlag[k]==0)
-                dispParameter(i,k,psr,fout,0,efac);
+                dispParameter(i,k,psr,fout,0,efac,useCompare,compareFile,&useDagger);
             }
         }
     }
@@ -171,6 +204,15 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
                        pow(psr[0].param[param_f].val[0],3))*3.2e19);
   fprintf(fout,"$\\log_{10}$(Surface magnetic field strength, G) \\dotfill & %.2f \\\\\n",log10(pval));
 
+  /* Edot */
+  pdot = psr[0].param[param_f].val[1]/pow(psr[0].param[param_f].val[0],2);
+  pval = (double)4.0/1.0e-15*M_PI*M_PI/pow(1.0/psr[0].param[param_f].val[0],3)*1e30*pdot;
+  printf("Have %g %g\n",pdot,pval);
+    //(sqrt(-psr[0].param[param_f].val[1]/
+    //                       pow(psr[0].param[param_f].val[0],3))*3.2e19);
+  fprintf(fout,"$\\log_{10}$(Edot, ergs/s) \\dotfill & %.2f \\\\\n",log10(fabs(pval)));
+
+
   /* Distance from parallax */
   if (psr[0].param[param_px].paramSet[0]==1){
     double pxdist,pxdistErr;
@@ -179,18 +221,55 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
 
     pxdist = (1.0/psr[0].param[param_px].val[0]*1000.0);
     pxdistErr = 1.0/powl(psr[0].param[param_px].val[0],2.0)*psr[0].param[param_px].err[0]*1000.0;
-    rnd8((double)pxdist,pxdistErr*efac,1,cval,&lv,cerr,&le,msg);
-    fprintf(fout,"Distance from parallax (pc) \\dotfill & %s(%s) \\\\\n",cval,cerr);
+    rnd8((double)pxdist/1000.0,pxdistErr*efac/1000.0,1,cval,&lv,cerr,&le,msg);
+    fprintf(fout,"Distance from parallax (kpc) \\dotfill & %s(%s) \\\\\n",cval,cerr);
   }
 
+  /* Distances from command line */
+  if (strcmp(dist1,"NULL")!=0)
+    fprintf(fout,"TC93 distance (kpc) \\dotfill & %s\\\\\n",dist1);
+  if (strcmp(dist2,"NULL")!=0)
+    fprintf(fout,"CL02 distance (kpc) \\dotfill & %s\\\\\n",dist2);
 
   fprintf(fout,"\\hline\n");
   fprintf(fout,"\\multicolumn{2}{c}{Assumptions} \\\\\n");
   fprintf(fout,"\\hline\n"); 
-  fprintf(fout,"Clock correction procedure\\dotfill & %s \\\\\n",psr[0].clock);
-  fprintf(fout,"Solar system ephemeris model\\dotfill & %s \\\\\n",
-          psr[0].ephemeris);
-  fprintf(fout,"Binary model\\dotfill & %s \\\\\n",psr[0].binaryModel);
+  if (useCompare==0)
+    {
+      fprintf(fout,"Clock correction procedure\\dotfill & %s \\\\\n",psr[0].clock);
+      fprintf(fout,"Solar system ephemeris model\\dotfill & %s \\\\\n",
+	      psr[0].ephemeris);
+      fprintf(fout,"Binary model\\dotfill & %s \\\\\n",psr[0].binaryModel);
+    }
+  else
+    {
+      char str[1024];
+      FILE *fin;
+      fin = fopen(compareFile,"r");
+      while (!feof(fin))
+	{
+	  if (fscanf(fin,"%s",str)==1)
+	    {
+	      if (strcmp(str,"EPHEM")==0)
+		{
+		  fscanf(fin,"%s",str);
+		  fprintf(fout,"Solar system ephemeris model\\dotfill & %s & %s \\\\\n",
+			  psr[0].ephemeris,str);
+		}
+	      else if (strcmp(str,"CLK")==0)
+		{
+		  fscanf(fin,"%s",str);
+		  fprintf(fout,"Clock correction procedure\\dotfill & %s & %s \\\\\n",psr[0].clock,str);
+		}
+	      else if (strcmp(str,"BINARY")==0)
+		{
+		  fscanf(fin,"%s",str);
+		  fprintf(fout,"Binary model\\dotfill & %s & %s\\\\\n",psr[0].binaryModel,str);
+		}
+	    }
+	}
+      fclose(fin);
+    }
   
   if (psr[0].calcShapiro==-1) 
     fprintf(fout,"Solar system Shapiro delay \\dotfill & N \\\\\n");
@@ -219,8 +298,10 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
 
   fprintf(fout,"\\hline\n");
   fprintf(fout,"\\end{tabular}\n");
+  if (useDagger==1)
+    fprintf(fout,"$\\dagger$ comparison made by updating the new parameters to the epoch of the previous parameters\n");
   if (nohead==1){
-    fprintf(fout,"\\end{minipage}\n");
+    fprintf(fout,"\\end{minipage}}\n");
   }
 
   if (efac==1)
@@ -235,17 +316,18 @@ extern "C" int tempoOutput(int argc,char *argv[],pulsar *psr,int npsr)
   printf("Suggest adding the following into your paper:\n\n");
   printf("Note: Figures in parentheses are %s the nominal 1$\\sigma$ \\textsc{tempo2} uncertainties in the least-significant digits quoted.\n",efacStr);
 
+  if (useCompare==1)
+    fprintf(fout,"\\end{table*}\n");
+  else
+    fprintf(fout,"\\end{table}\n");
   if (nohead==0)
-    {
-      fprintf(fout,"\\end{table}\n");
-      
+    {      
       fprintf(fout,"\\end{document}\n");
     }
   fclose(fout);
 }
 
-
-void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac)
+void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac,int useCompare,char *compareFile,int *useDagger)
 {
   char label[1000];
   char valStr[1000];
@@ -256,53 +338,13 @@ void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac)
     {
       strcpy(valStr,psr[0].rajStrPost);
       if (err==1)
-        {
-          int dp,ierr,sym,k;
-          double err;
-          char disp[500];
-          
-          strcpy(disp,psr[0].rajStrPost);
-          err = psr[0].param[i].err[0]*(efac);
-          dp = nint_derived(log10(err*3600.0/M_PI*12.0));
-          if (err*3600.0/M_PI*12.0/pow(10.0,(double)dp)<1.90)
-            {
-              if (dp < 0) dp --;
-              else dp ++;
-            }
-          ierr = (int)(err*3600.0/M_PI*12.0/pow(10.0,(double)dp)+0.9999);
-          for (k=0;k<strlen(disp);k++)
-            {
-              if (disp[k] == ':') sym++;
-              if (disp[k] == '.' && dp < 0) {disp[k-dp+1]='\0'; break;}
-            }
-          sprintf(valStr,"%s(%d)",disp,ierr);
-        }
+	fixRA(psr[0].rajStrPost,psr[0].param[i].err[0]*efac,valStr);
     }
   else if (i==param_decj)
     {
       strcpy(valStr,psr[0].decjStrPost);
       if (err==1)
-        {
-          int dp,ierr,sym,k;
-          double err;
-          char disp[500];
-          
-          strcpy(disp,psr[0].decjStrPost);
-          err = psr[0].param[i].err[0]*(efac);
-          dp = nint_derived(log10(err*3600.0/M_PI*180.0));
-          if (err*3600.0/M_PI*180.0/pow(10.0,(double)dp)<1.90)
-            {
-              if (dp < 0) dp --;
-              else dp ++;
-            }
-          ierr = (int)(err*3600.0/M_PI*180.0/pow(10.0,(double)dp)+0.9999);
-          for (k=0;k<strlen(disp);k++)
-            {
-              if (disp[k] == ':') sym++;
-              if (disp[k] == '.' && dp < 0) {disp[k-dp+1]='\0'; break;}
-            }
-          sprintf(valStr,"%s(%d)",disp,ierr);
-        }
+	fixDec(psr[0].decjStrPost,psr[0].param[i].err[0]*efac,valStr);
     }
   else
     {
@@ -314,7 +356,7 @@ void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac)
         }
       else
         sprintf(valStr,"%Lg",psr[0].param[i].val[k]);
-    }
+   }
   parseMinus(valStr);
   parseExp(valStr);
   
@@ -371,10 +413,211 @@ void dispParameter(int i,int k,pulsar *psr,FILE *fout,int err,double efac)
   else
     sprintf(label,"%s",psr[0].param[i].label[k]);
 
-  fprintf(fout,"%s\\dotfill & %s \\\\ \n",label,valStr);
-  
+  if (useCompare==1)
+    {
+      FILE *fin;
+      char catParam[128];
+      char line[128];
+      double catVal,catErr;
+      char catStr[1024],catValStr[128];
+      int nread;
+      int match;
+      double change;
+      double raVal1,decVal1,raVal2,decVal2;
+      double thisNewEpochVal;
+      double diff,errDiff;
+      double epoch1,epoch2,epoch2_p,epoch2_dm,epoch2_pos;
+      int changeEpoch;
+      char changeStr[128];
+
+      epoch2_p = epoch2_dm = epoch2_pos = -1;
+      match=0;
+      changeEpoch=0;
+
+      // Find the epochs
+      fin = fopen(compareFile,"r");
+      while (!feof(fin))
+	{
+	  fscanf(fin,"%s",catParam);
+	  if (strcmp(catParam,"PEPOCH")==0)
+	    fscanf(fin,"%lf",&epoch2_p);
+	  else if (strcmp(catParam,"DMEPOCH")==0)
+	    fscanf(fin,"%lf",&epoch2_dm);
+	  else if (strcmp(catParam,"POSEPOCH")==0)
+	    fscanf(fin,"%lf",&epoch2_pos);
+	}
+      fclose(fin);
+
+      fin = fopen(compareFile,"r");
+      while (!feof(fin))
+	{
+	  fgets(line,128,fin);
+	  nread = sscanf(line,"%s %s %lf",catParam,catValStr,&catErr);
+	  if (nread > 0)
+	    {
+	      if (strcmp(catParam,psr[0].param[i].shortlabel[k])==0)
+		{
+		  //		  printf("GOT A MATCH: %s\n",catParam);
+		  match=1;
+		  
+		  if (nread==3)
+		    {
+		      // Check for RAJ
+		      if (i==param_raj)
+			{
+			  // Error in arcsec
+			  catErr *= M_PI/12.0/60.0/60.0;
+			  raVal2 = fixRA(catValStr,catErr,catStr);			  
+			  
+			  //			  printf("raVal = %g %g %g\n",raVal2,catErr,(double)psr[0].param[param_raj].val[0]);
+			}
+		      else if (i==param_decj)
+			{
+			  catErr *= M_PI/180.0/60.0/60.0;
+			  decVal2 = fixDec(catValStr,catErr,catStr);			  
+			}
+		      else
+			{			  
+			  sscanf(catValStr,"%lf",&catVal);
+			  rnd8(catVal,catErr,1,cval,&lv,cerr,&le,msg);
+			  sprintf(catStr,"%s(%s)",cval,cerr);
+			}
+		      // Calculate change
+		      if (i==param_f && k==0) {
+			epoch1 = (double)psr[0].param[param_pepoch].val[0];
+			epoch2 = epoch2_p;
+			thisNewEpochVal = (double)psr[0].param[i].val[k]+
+			  (epoch2-epoch1)*SECDAY*psr[0].param[param_f].val[1];
+			changeEpoch=1;
+		      }
+		      else if (i==param_raj)
+			{
+			  epoch1 = (double)psr[0].param[param_posepoch].val[0];
+			  if (epoch2_pos==-1)
+			    epoch2 = epoch2_p;
+			  else
+			    epoch2 = epoch2_pos;
+
+			  thisNewEpochVal = (double)psr[0].param[param_raj].val[0]+
+			    (epoch2-epoch1)*SECDAY*psr[0].param[param_pmra].val[0]/cos(psr[0].param[param_decj].val[0])*M_PI/180.0/1000.0/365.25/SECDAY/60.0/60.0;
+			  changeEpoch=1;
+			  catVal = raVal2;
+			  printf("Testing %f %f %f\n",thisNewEpochVal,catVal,catErr);
+			}
+		      else if (i==param_decj)
+			{
+			  epoch1 = (double)psr[0].param[param_posepoch].val[0];
+			  if (epoch2_pos==-1)
+			    epoch2 = epoch2_p;
+			  else
+			    epoch2 = epoch2_pos;
+
+			  thisNewEpochVal = (double)psr[0].param[param_decj].val[0]+
+			    (epoch2-epoch1)*SECDAY*psr[0].param[param_pmdec].val[0]*M_PI/180.0/1000.0/365.25/SECDAY/60.0/60.0;
+			  changeEpoch=1;
+			  catVal = decVal2;
+			  printf("Testing %.10f %.10f %.10f %f\n",(double)psr[0].param[param_decj].val[0],thisNewEpochVal,catVal,catErr);
+			}
+		      else
+			thisNewEpochVal = (double)psr[0].param[i].val[k];
+		    
+		      diff = (thisNewEpochVal-catVal);
+		      errDiff = sqrt(pow(psr[0].param[i].err[k],2) + pow(catErr,2));
+		      change=diff/errDiff;
+		      sprintf(changeStr,"%.1f",change);
+		      parseMinus(changeStr);
+		    }
+		  else
+		    sprintf(catStr,"%s",catValStr);
+	  //	  printf("Trying %g %g %g %s %s\n",(double)psr[0].param[i].val[k],(double)psr[0].param[i].err[k],(double)efac,cval,cerr);
+
+
+		  break;
+		}
+	    }
+	}
+      fclose(fin);
+      parseMinus(catStr);
+      parseExp(catStr);
+
+      if (match==1)
+	{
+	  if (nread==3) 	    	   
+	    {
+	      if (changeEpoch==1)
+		{
+		  fprintf(fout,"%s\\dotfill & %s & %s & %s$^\\dagger$ \\\\ \n",label,valStr,catStr,changeStr);
+		  *useDagger=1;
+		}
+	      else
+		fprintf(fout,"%s\\dotfill & %s & %s & %s \\\\ \n",label,valStr,catStr,changeStr);
+	    }
+	  else
+	    fprintf(fout,"%s\\dotfill & %s & %s & -- \\\\ \n",label,valStr,catStr);
+	}
+      else
+	fprintf(fout,"%s\\dotfill & %s & -- & -- \\\\ \n",label,valStr);
+    }
+  else
+    fprintf(fout,"%s\\dotfill & %s \\\\ \n",label,valStr);
 }
 
+
+double fixRA(char *tstr,double err,char *valStr)
+{
+  int dp,ierr,sym,k;
+  double hr,min,sec;
+  char disp[500];
+  double ra;
+
+  sscanf(tstr,"%lf:%lf:%lf",&hr,&min,&sec);
+  ra = (hr+min/60.0+sec/3600.0)/12.0*M_PI;
+  
+  strcpy(disp,tstr);
+  dp = nint_derived(log10(err*3600.0/M_PI*12.0));
+  if (err*3600.0/M_PI*12.0/pow(10.0,(double)dp)<1.90)
+    {
+      if (dp < 0) dp --;
+      else dp ++;
+    }
+  ierr = (int)(err*3600.0/M_PI*12.0/pow(10.0,(double)dp)+0.9999);
+  for (k=0;k<strlen(disp);k++)
+    {
+      if (disp[k] == ':') sym++;
+      if (disp[k] == '.' && dp < 0) {disp[k-dp+1]='\0'; break;}
+    }
+  sprintf(valStr,"%s(%d)",disp,ierr);
+
+  return ra;
+}
+
+double fixDec(char *tstr,double err,char *valStr)
+{
+  int dp,ierr,sym,k;
+  char disp[500];
+  double deg,min,sec;
+  double dec;
+
+  sscanf(tstr,"%lf:%lf:%lf",&deg,&min,&sec);
+  dec = (fabs(deg)+min/60.0+sec/3600.0)/180.0*M_PI;
+  if (deg < 0) dec=-dec;
+
+  strcpy(disp,tstr);
+  dp = nint_derived(log10(err*3600.0/M_PI*180.0));
+  if (err*3600.0/M_PI*180.0/pow(10.0,(double)dp)<1.90)
+    {
+      if (dp < 0) dp --;
+      else dp ++;
+    }
+  ierr = (int)(err*3600.0/M_PI*180.0/pow(10.0,(double)dp)+0.9999);
+  for (k=0;k<strlen(disp);k++)
+    {
+      if (disp[k] == ':') sym++;
+      if (disp[k] == '.' && dp < 0) {disp[k-dp+1]='\0'; break;}
+    }
+  sprintf(valStr,"%s(%d)",disp,ierr);
+  return dec;
+}
 
 int rnd8(double rval,double rerr,int ifac,char *cval,int *lv,char *cerr,int *le,char *msg)
 {
