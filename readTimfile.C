6087c110fa07d9783fd2fef6e14caf6c509d6e5c
sosl <>
2014-07-15 13:31:24 +0000

    More portable version of the buffer overflow fix

--- readTimfile.C
+++ readTimfile.C
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <linux/limits.h>
 #include "tempo2.h"
 
 /* ******************************************** */
@@ -551,13 +552,21 @@ void readTim(char *timname,pulsar *psr,int *jumpVal)
 	      else if (strcasecmp(param1,"INCLUDE")==0) /* Include another .tim file */
 		{
 		  char newtim[MAX_FILELEN];
+#ifdef PATH_MAX
+		  char relPath[PATH_MAX];
+#else
 		  char *relPath;
+#endif
 		  if (sscanf(line,"%s %s",param1,newtim)==2)
 		    {
 		      int ii;
 		      // Relative file path
 		      printf("Current filename = %s\n",timname);
+#ifdef PATH_MAX
+		      realpath(timname,relPath);
+#else
 		      relPath = realpath(timname,NULL);
+#endif
 		      // Remove filename
 		      for (ii=strlen(relPath);ii>0;ii--)
 			{
@@ -571,7 +580,9 @@ void readTim(char *timname,pulsar *psr,int *jumpVal)
 		      printf("Rel path = %s\n",relPath);
 		      //		      readTim(newtim,psr,jumpVal);
 		      readTim(relPath,psr,jumpVal);
+#ifndef PATH_MAX
 		      free(relPath);
+#endif
 		    }
 		  else
 		    printf("Unable to parse INCLUDE line >%s<\n",line);
