76049bad38248513ebffc3741146d7e0acc3e111
vhaasteren <>
2014-07-16 18:49:11 +0000

    FIXED: limits.h should be included without 'linux', just as in clkcorr.

--- readTimfile.C
+++ readTimfile.C
@@ -31,7 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <linux/limits.h>
+#include <limits.h>
 #include "tempo2.h"
 
 /* ******************************************** */
