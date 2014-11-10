1eb4f0e80662e8167cb924b5a5378e0f7eba4dc3
sixbynine <>
2014-11-10 21:20:34 +0000

    Try an aligned malloc for 2d float matricies

--- TKmatrix.C
+++ TKmatrix.C
@@ -189,7 +189,12 @@ float** malloc_2df(int rows,int cols){
    logdbg("Allocate %d x %d float array (%.3f kb)",rows,cols, (double)(rows*cols*sizeof(float)/1024.0));
 
    m=(float**) calloc(rows,sizeof(float*));
-   memory=(float*)calloc(rows*cols,sizeof(float));
+   //memory=(float*)malloc(sizeof(float)*rows*cols);
+   posix_memalign((void**)&memory,16,sizeof(float)*rows*cols);
+
+   for(i=0;i<rows*cols;i++){
+	  memory[i]=0;
+   }
 
    if(memory==NULL || m==NULL){
 	  logdbg("Could not allocate %d x %d float array (%.3f kb)",rows,cols, (double)(rows*cols*sizeof(float)/1024.0));
