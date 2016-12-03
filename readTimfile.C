mattpitkin/tempo2

commit bb6933e4a4f27da4983d974152aee499ef1cbcbd

Fix for the last flag at end of line

readTimfile.C
@@ -250,6 +250,9 @@ void readTim(char *timname,pulsar *psr,int *jumpVal)
 
                     /*		  strcpy(psr->obsn[nObs].flagID[0],"FLAGID");
                               strcpy(psr->obsn[nObs].flagVal[0],"FLAGVAL"); */
+
+                    // set the last char to a whitespace to prevent getting the newline char into the last flag value
+                    line[(int)strlen(line)-1]=' ';
                     for (i=0;i<(int)strlen(line)-1;i++)
                     {
                         if (line[i]=='-' && (line[i+1] < 48 || line[i+1] > 57))

commit c23c187bc2ea0b37a7d3f165efe78b78bf59c07b

Fix for end of line issue introduced in bb6933e

configure.ac
@@ -2,7 +2,7 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ(2.57)
-AC_INIT([Tempo2],[2016.11.0],[george.hobbs@csiro.au],[],[http://www.bitbucket.org/psrsoft/tempo2])
+AC_INIT([Tempo2],[2016.11.2],[george.hobbs@csiro.au],[],[http://www.bitbucket.org/psrsoft/tempo2])
 
 AC_ARG_VAR([TEMPO2], [Default TEMPO2 installation directory])
 AC_PREFIX_DEFAULT([${TEMPO2:-"/usr/local"}])

readTimfile.C
@@ -196,8 +196,8 @@ void readTim(char *timname,pulsar *psr,int *jumpVal)
                 valid=-2;
             //	  printf("Have read %s\n",line);
 
-            /* Remove \n character at end */
-            if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';
+            /* Remove whitespace (or eol) characters at end of line */
+            while (isspace(line[strlen(line)-1])) line[strlen(line)-1]='\0';
             if (line[0]!='C' && line[0]!='#') /* Ignore comment lines     */
             {
                 /* Read default columns */
@@ -251,8 +251,6 @@ void readTim(char *timname,pulsar *psr,int *jumpVal)
                     /*		  strcpy(psr->obsn[nObs].flagID[0],"FLAGID");
                               strcpy(psr->obsn[nObs].flagVal[0],"FLAGVAL"); */
 
-                    // set the last char to a whitespace to prevent getting the newline char into the last flag value
-                    line[(int)strlen(line)-1]=' ';
                     for (i=0;i<(int)strlen(line)-1;i++)
                     {
                         if (line[i]=='-' && (line[i+1] < 48 || line[i+1] > 57))