efb953f0437b884fa66579522fc3543c5a80933a
Michael Keith <mkeith@pulsarastronomy.net>
2017-03-08 10:44:04 +0000

    Udpdating toasim code
    Former-commit-id: a0047cbc951111ed47d786d8f72163e8debb77ab

--- unsupported_plugins/toasim/python/toasim.py
+++ unsupported_plugins/toasim/python/toasim.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python
 import struct
 from array import array
+import numpy as np
+import astropy.units as u
+import astropy.coordinates as coord
+
+
+class ToA:
+    def __init__(self,toa,freq,error,site,flags=dict(),name="X"):
+        e=toa.split(".")
+        self.toa = (np.longdouble(e[0])+ np.longdouble("0."+e[1]))*u.day
+        self.freq = freq.to(u.megahertz)
+        self.site = site
+        self.flags=flags
+        self.error = error.to(u.microsecond)
+        self.name=name
+
+    @staticmethod
+    def ld_to_string(toa):
+        intpart = int(np.floor(toa))
+        floatpart = toa - np.longdouble(intpart)
+        str1="%d"%intpart
+        str2="%.20f"%(floatpart)
+        return "%s%s"%(str1,str2[1:])
+
+    def __repr__(self):
+        return "%s %f %f"%(ToA.ld_to_string(self.toa.value),self.error.value,self.freq.value)
+
+    def __str__(self):
+        return "%s %f %f"%(ToA.ld_to_string(self.toa.value),self.error.value,self.freq.value)
 
 class correction:
     def __init__(self,header,offsets,a0,a1,a2,params):
@@ -39,6 +67,59 @@ class header:
         self.nrealisations=0
         self.d_start=0
         self.d_offset=0
+
+    def readParTim(self):
+        with open(self.parfile_name) as f:
+            self.orig_parfile = f.read()
+        with open(self.timfile_name) as f:
+            self.idealised_toas = f.read()
+
+
+        toas=list()
+
+        first=True
+        t2format=False
+        for line in self.idealised_toas.split("\n"):
+            if line.startswith("C") or line.startswith("#"):
+                continue
+            line = line.strip()
+            if first and line.startswith("FORMAT 1"):
+                first=False
+                t2format=True
+            first=False
+            elems=line.split()
+            if len(elems) < 5:
+                continue
+            if t2format:
+                flags=dict()
+                i=5
+                while (i+1) < len(elems):
+                    flags[elems[i]] = elems[i+1]
+                    i+=2
+                toas.append(ToA(toa=elems[2],  \
+                                freq=float(elems[1])*u.megahertz,   \
+                                error=float(elems[3])*u.microsecond,\
+                                site=elems[4],                      \
+                                flags=flags,name=elems[0]))
+
+
+
+        par = dict()
+        for line in self.orig_parfile.split("\n"):
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            elems = line.split()
+            if len(elems) < 2:
+                continue
+            key=elems[0]
+            val=elems[1]
+
+            par[key] = val
+
+        if "RAJ" in par and "DECJ" in par:
+            par['coord'] = coord.SkyCoord(ra=par['RAJ'],dec=par['DECJ'], unit=(u.hourangle,u.degree))
+        return toas,par
 
     def write(self,stream):
         self.version=header.VERSION
