1bd96719889a31c4b8c166966545929522a4251f
Michael Keith <mkeith@pulsarastronomy.net>
2017-08-15 18:19:03 +0100

    Fixes for ifunc fitting and removal of duplicate code

--- t2fit_ifunc.h
+++ t2fit_ifunc.h
@@ -1,6 +1,4 @@
 #include "tempo2.h"
-double ifunc(const double* mjd, const double t,const int N, const int k);
-double sinfunc(const double *T, const double t, const int k);
 
 
 double t2FitFunc_sifunc(pulsar *psr, int ipsr ,double x ,int ipos ,param_label label,int k);
