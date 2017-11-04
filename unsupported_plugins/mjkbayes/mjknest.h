cd565fd9fb6c89ab70a9bae9f1a4aae465002406
Michael Keith <mkeith@pulsarastronomy.net>
2017-11-04 20:17:49 +0000

    Added configuration file for mjkbayes
    Former-commit-id: d1b6654f4144817ece2623ee0fcae1339dae090a

--- unsupported_plugins/mjkbayes/mjknest.h
+++ unsupported_plugins/mjkbayes/mjknest.h
@@ -2,6 +2,8 @@
 
 #define FITTYPE_PARAM 0
 #define FITTYPE_CHOL 1
+#define FITTYPE_EFAC 2
+#define FITTYPE_EQUAD 3
 
 #define FITTYPE_CHOL_K_AMP 0
 #define FITTYPE_CHOL_K_ALPHA 1
@@ -17,6 +19,7 @@ struct mjkcontext {
     double fitoffset[1024];
     pulsar* psr;
     FILE* debugfile;
+    char* flagmask[1024];
 };
 
 
@@ -24,3 +27,7 @@ struct mjkcontext {
 void LogLike(double *Cube, int &ndim, int &npars, double &lnew, void *context);
 
 void dumper(int &nSamples, int &nlive, int &nPar, double **physLive, double **posterior, double **paramConstr, double &maxLogLike, double &logZ, double &INSlogZ, double &o, void *context);
+
+void loadmjkbayescfg(const char* cfg, pulsar* psr, mjkcontext *context);
+
+char* mjkbayesflagmask(pulsar* psr, const char* flag, const char* flagval);
