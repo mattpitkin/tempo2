438e7a6b30c721bd43288f15129540c37a7a06ab
Michael Keith <mkeith@pulsarastronomy.net>
2017-11-29 09:53:32 +0000

    Working on output for MJKBayes
    Former-commit-id: ce07aa3b97931651bb6d62f54b03b57c8d90e087

--- unsupported_plugins/mjkbayes/make_mjkbayes.sh
+++ unsupported_plugins/mjkbayes/make_mjkbayes.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ARCH=linux-gnu
+#ARCH=linux-gnu
+ARCH=darwin15
 
-CXX=g++
+CXX=g++-7
 
-cmd="$CXX -o $TEMPO2/plugins/mjkbayes_${ARCH}_plug.t2 mjkbayes_plug.C -ltempo2 -I../.. $LDFLAGS $CFLAGS -shared -fPIC -O2 -lmultinest_mpi"
+cmd="$CXX -o $TEMPO2/plugins/mjkbayes_${ARCH}_plug.t2 mjkbayes_plug.C -ltempo2 -I../.. $LDFLAGS $CFLAGS -shared -fPIC -O2 /Users/mkeith/Code/tempo2/TempoNest/MultiNest/libnest3.a -lgfortran -llapack -lmpi -lmpi_mpifh"
 
 echo $cmd
 $cmd
