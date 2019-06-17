ef28adfe0ed027909f4265f4372c27e272bc8ee3
Michael Keith <mkeith@pulsarastronomy.net>
2019-06-17 11:14:10 +0100

    Fix issue with picky C compilers
    Former-commit-id: 23edf6cc975476a66bcda8fb077b802fbc79c027

--- python/t2pred/t2pred_loops.c
+++ python/t2pred/t2pred_loops.c
@@ -2,12 +2,14 @@
 #include "t2pred_loops.h"
 
 void T2Predictor_GetPhase_array_ld(const T2Predictor *t2p, long double *mjd, const int nmjd, long double freq, double* out) {
-    for (int i=0; i < nmjd; ++i){
+    int i;
+    for (i=0; i < nmjd; ++i){
         out[i] = T2Predictor_GetPhase(t2p,mjd[i],freq);
     }
 }
 void T2Predictor_GetFrequency_array_ld(const T2Predictor *t2p, long double *mjd, const int nmjd, long double freq, double* out) {
-    for (int i=0; i < nmjd; ++i){
+    int i;
+    for (i=0; i < nmjd; ++i){
         out[i] = T2Predictor_GetFrequency(t2p,mjd[i],freq);
     }
 }
@@ -15,12 +17,14 @@ void T2Predictor_GetFrequency_array_ld(const T2Predictor *t2p, long double *mjd,
 
 
 void T2Predictor_GetPhase_array(const T2Predictor *t2p, double refmjd, double *mjd, const int nmjd, double freq, double* out) {
-    for (int i=0; i < nmjd; ++i){
+    int i;
+    for (i=0; i < nmjd; ++i){
         out[i] = T2Predictor_GetPhase(t2p,mjd[i],freq);
     }
 }
 void T2Predictor_GetFrequency_array(const T2Predictor *t2p, double refmjd, double *mjd, const int nmjd, double freq, double* out) {
-    for (int i=0; i < nmjd; ++i){
+    int i;
+    for (i=0; i < nmjd; ++i){
         out[i] = T2Predictor_GetFrequency(t2p,mjd[i],freq);
     }
 }
