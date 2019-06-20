0516bf5aae26b242df9886a31ec4b25c21da7c9e
Michael Keith <mkeith@pulsarastronomy.net>
2019-06-20 10:26:04 +0100

    Moved toasim python code and updated a little bit
    Former-commit-id: 080c340f5736e7901eac03c03139b0ec49c0a4cc

---
+++ python/toasim/bin/fake_lunatic.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+
+import sys
+from astropy import coordinates as coord
+from astropy import units as u
+from astropy import time as atime
+from astropy import constants as const
+import numpy as np
+
+import toasim
+
+nreal = 1
+header = toasim.header()
+
+header.parfile_name=sys.argv[1]
+header.timfile_name=sys.argv[2]
+
+
+with open(sys.argv[1]) as par:
+    header.orig_parfile=par.read()
+with open(sys.argv[2]) as tim:
+    header.idealised_toas=tim.read()
+
+
+
+
+with open(header.parfile_name) as f:
+    for line in f:
+        e=line.split()
+        if e[0]=="RAJ":
+            raj=e[1]
+        elif e[0]=="DECJ":
+            decj=e[1]
+
+toas=[]
+with open(header.timfile_name) as f:
+    for line in f:
+        if line.startswith("C") or line.startswith("#"):
+            continue
+        e=line.split()
+        if len(e) > 4:
+            toas.append(float(e[2]))
+
+psrpos=coord.SkyCoord(raj,decj,unit=(u.hourangle,u.degree))
+
+t = atime.Time(toas,format='mjd')
+moon=coord.get_moon(t)
+
+oproduct = psrpos.cartesian.dot(moon.cartesian)
+
+mass_ratio = 7.3476e22 / 5.972e24
+
+path_delay = (oproduct/const.c).to(u.s).value*mass_ratio
+
+header.ntoa=len(path_delay)
+header.nrealisations=nreal
+real = toasim.correction(header,path_delay,0,0,0,"")
+with open(header.timfile_name+".noMoon","wb") as f:
+    header.write(f)
+    real.write(f)
+
+
