bd6de63c9933a78381ea25c8b3bd1bd387cceec8
Michael Keith <michael.keith@manchester.ac.uk>
2020-11-11 16:30:59 +0000

    Update tests for new shapiro code.
    Small deviations due to change of shapiro sign invalidated tests.
    
    Added "1937_2020" test with many parameters and telescopes and latest
    models and ephemeris

--- tests/testEndToEnd.C
+++ tests/testEndToEnd.C
@@ -102,6 +102,39 @@ TEST(testEndToEnd,checkDE430){
 
 }
 
+TEST(testEndToEnd,check_1937_2020){
+    pulsar _psr;
+    pulsar *psr = &_psr;
+    MAX_PSR=1;
+    char timFile[MAX_PSR][MAX_FILELEN],parFile[MAX_PSR][MAX_FILELEN];
+    int npsr=1;
+    initialise(psr,0); /* Initialise all */
+
+    strcpy(parFile[0],DATDIR "/test_1937_2020.par");
+    strcpy(timFile[0],DATDIR "/test_1937_2020.tim");
+
+    readParfile(psr,parFile,timFile,npsr);
+    readTimfile(psr,timFile,npsr);
+    preProcessSimple(psr);
+
+    psr->noWarnings=2;
+    formBatsAll(psr,npsr);
+    formResiduals(psr,npsr,0);
+    for(int iobs = 0; iobs < psr->nobs; iobs++){
+        ASSERT_LT(static_cast<double>(fabsl(psr->obsn[iobs].residual)),TEST_DELTA) << DATDIR "/test_de430.par test_de430.tim do not give idealised ToAs";
+    }
+
+    t2Fit(psr,npsr,"NULL");
+
+    formBatsAll(psr,npsr);
+    formResiduals(psr,npsr,0);
+    for(int iobs = 0; iobs < psr->nobs; iobs++){
+        ASSERT_LT(static_cast<double>(fabsl(psr->obsn[iobs].residual)),TEST_DELTA) << "Fitting has caused error in ideal";
+    }
+
+}
+
+
 
 
 TEST(testEndToEnd,checkFit){
