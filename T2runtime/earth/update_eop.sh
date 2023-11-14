c363cd819e43b4062439aadcc37ce46690d59d0f
Michael Keith <michael.keith@manchester.ac.uk>
2023-11-14 10:18:05 +0000

    Fix to extrapolation for BIPM2020/21. Update EOP. Add 2022
    Former-commit-id: 3610511302af0f6f62d2a9806177301516a9babf

--- T2runtime/earth/update_eop.sh
+++ T2runtime/earth/update_eop.sh
@@ -6,7 +6,7 @@ echo "Attempt to update earth orientation parameters"
 
 echo "before:"
 tail -n 3 eopc04_IAU2000.62-now
-wget -N ftp://hpiers.obspm.fr/iers/eop/eopc04/eopc04_IAU2000.62-now
+wget -N https://hpiers.obspm.fr/iers/eop/eopc04/eopc04_IAU2000.62-now
 echo ""
 echo "after:"
 tail -n 3 eopc04_IAU2000.62-now
